6247498b1a99bcbe6778a4e6462e26df865f1604
이호섭 <hoossky2@gmail.com>
2020-07-29 17:19:28 +0900

    "17:19"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 export {default as Notice} from './Notice'
 export {default as Community} from './Community'
 export {default as Contacts} from './Contacts'
-export {default as Events} from './Events'
 export {default as Main} from './Main'

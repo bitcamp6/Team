39cc874cac5b89fcd52be1fe474c2a73cd24ed00
양정욱 <jayden6830@gmail.com>
2020-07-31 10:36:15 +0900

    31/07/2020 Address API

--- src/pages/Account/Postcode.jsx
+++ src/pages/Account/Postcode.jsx
@@ -23,7 +23,9 @@ const Postcode = ({onSelectedAddr}) => {
             }
             fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
         }
-        console.log(fullAddress);  // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+            onSelectedAddr(fullAddress)
+            setShow(false)
+            console.log(fullAddress);  // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
     }
 
     return (
